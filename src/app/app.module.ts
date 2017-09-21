6c822aacb6f0cd8e61e5a027d195eb95e52bdabb
djianLucca <djianlucca@outlook.com>
2017-09-21 15:49:58 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
+import { CursosModule } from './cursos/cursos.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuSegundoComponent } from './meu-segundo/meu-segundo.component';
+import { CursosModule } from './meu-segundo/meu-segundo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuSegundoComponent,
+    CursosModule
   ],
   imports: [
     BrowserModule
---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import{ Component } from '@angular/core';
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu primeiro template de angular 2!</p>
+    `
+})
+export class MeuPrimeiroComponent{
+
+ }
